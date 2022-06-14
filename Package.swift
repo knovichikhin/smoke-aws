2fba3a43aeb6a1b1adef38af429297482d28b09c
Simon Pilkington <simonpi@amazon.com>
2022-06-15 06:48:33 +1000

    Rename package.

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "smoke-aws-infrastructure-constructs",
-            targets: ["smoke-aws-infrastructure-constructs"]),
+            name: "SmokeAWSInfrastructureConstructs",
+            targets: ["SmokeAWSInfrastructureConstructs"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "smoke-aws-infrastructure-constructs",
+            name: "SmokeAWSInfrastructureConstructs",
             dependencies: [
                 .product(name: "Algorithms", package: "swift-algorithms"),
             ]),
--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { GithubSourcedSwiftLambdaWithFunctionUriStack } from '../lib/github-sourced-swift-lambda-with-function-uri-stack';
 
