09ae26766f9a204f2f1830746a8b8c641c5164dd
Simon Pilkington <simonpi@amazon.com>
2022-06-15 10:35:46 +1000

    Run script in container.

--- infrastructure/lib/github-sourced-pipeline-stack.ts
+++ infrastructure/lib/github-sourced-pipeline-stack.ts
@@ -75,12 +75,10 @@ export class GithubSourcedPipelineStack extends Stack {
       `echo "Running ${infrastructureExecutableName} using Swift compiler version ${swiftVersion}"`,
 
       `SWIFT_DOCKER_IMAGE=${swiftDockerImage}`,
-      `INFRASTRUCTURE_RUN_COMMAND="swift run -c release ${infrastructureExecutableName}"`,
+      `INFRASTRUCTURE_RUN_COMMAND="swift run -c release ${infrastructureExecutableName} && .infrastructure_generated/script"`,
       'docker run --rm -v "$(pwd)":/workspace -w /workspace ${SWIFT_DOCKER_IMAGE} bash -cl "${INFRASTRUCTURE_RUN_COMMAND}"',
       
-      'ls',
       'CURRENT_DIRECTORY=$(pwd)',
-      '${CURRENT_DIRECTORY}/.infrastructure_generated/script',
       'cd ${CURRENT_DIRECTORY}/.infrastructure',
       'ls',
 
